f11fb81da0c83e04f93460d50e8a9c2fb46795f3
osmuntsun <osmuntsun900606@gmail.com>
2025-08-18 22:54:50 +0800

    獲得登入後cookies以及透過cookies來直接登入FB

--- Facebook/Facebook.py
+++ Facebook/Facebook.py
@@ -1,69 +1,62 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import os 
 from DownloadChromedriver import get_chromedriver
 import time
 import requests
+import json
 
 
 if not os.path.exists("chromedriver.exe"):
     get_chromedriver('139.0.7258.128')
 
+def FristLogin():
+    driver = webdriver.Chrome()
+    driver.get("https://www.facebook.com/?locale=zh_TW")
+    # 登入帳號
+    element = driver.find_element('xpath','//*[@id="email"]')
+    element.send_keys('osmuntsun900606@gmail.com') # 輸入帳號
+    element = driver.find_element('xpath','//*[@id="pass"]')
+    element.send_keys('osmunt900606') # 輸入密碼
 
-driver = webdriver.Chrome()
-driver.get("https://www.facebook.com/?locale=zh_TW")
-# 登入帳號
-element = driver.find_element('xpath','//*[@id="email"]')
-element.send_keys('osmuntsun900606@gmail.com') # 輸入帳號
-element = driver.find_element('xpath','//*[@id="pass"]')
-element.send_keys('osmunt900606') # 輸入密碼
+    time.sleep(2) # 等待2秒
+    driver.find_element('xpath','/html/body/div[1]/div[1]/div[1]/div/div/div/div[2]/div/div[1]/form/div[2]/button').click() # 點擊登入按鈕
+    WebDriverWait(driver, 50000).until(
+        EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div[2]/div/div/div/div[2]/div/div[2]/div/div/div/div[1]/div'))
+    )
+    cookie = driver.get_cookies() # 獲取cookies
+    with open('cookies.txt', 'w') as f: # 將cookies寫入文件
+        for c in cookie:
+            f.write(f"{c['name']}={c['value']}; \n")
+    print("Cookies have been saved to cookies.txt")
+    cookies_key = ['datr', 'sb', 'dpr', 'locale', 'c_user', 'xs', 'fr', 'wd', 'presence']
 
-time.sleep(2) # 等待2秒
-driver.find_element('xpath','/html/body/div[1]/div[1]/div[1]/div/div/div/div[2]/div/div[1]/form/div[2]/button').click() # 點擊登入按鈕
-
-input("請按Enter鍵繼續...") # 等待使用者按Enter鍵
-
-cookie = driver.get_cookies() # 獲取cookies
-print(cookie) # 打印cookies
-with open('cookies.txt', 'w') as f: # 將cookies寫入文件
+    cookies_1 = {}
     for c in cookie:
-        f.write(f"{c['name']}={c['value']}; ")
+        if c['name'] in cookies_key:
+            cookies_1[c['name']] = c['value'] # 指定cookies的名稱和值
+
+    with open('cookies.json', 'w', encoding='utf-8') as f:
+        json.dump(cookies_1, f, ensure_ascii=False, indent=4)
 
 
-cookie_names = [
-    "datr", "sb", "dpr", "locale", "ps_l", "ps_n", "c_user", "xs", "fr", "presence", "wd"
-]
 
-with open("cookies.txt", "r", encoding="utf-8") as f:
-    lines = f.readlines()
 
-# 合併所有 cookie 字串
-cookie_str = "".join(lines).replace("\n", "").replace("\r", "")
 
-# 解析成 dict
-cookie_dict = {}
-for item in cookie_str.split(";"):
-    item = item.strip()
-    if "=" in item:
-        k, v = item.split("=", 1)
-        cookie_dict[k.strip()] = v.strip()
+if not os.path.exists("cookies.json"):
+    FristLogin()
+else:
+    with open('cookies.json', 'r', encoding='utf-8') as f:
+        cookies_1 = json.load(f)
+    driver = webdriver.Chrome()
+    driver.get("https://www.facebook.com/")
+    for name, value in cookies_1.items():
+        driver.add_cookie({'name': name, 'value': value})
+    driver.refresh()
+    input("Cookies have been loaded. Press Enter to continue...")
 
-# 按指定順序組合
-result = []
-for name in cookie_names:
-    if name in cookie_dict:
-        result.append(f"{name}={cookie_dict[name]}")
-
-final_cookie = "; ".join(result)
-print(final_cookie)
-
-'''
-datr= 'value': 'UeWhaFwAEjstko2t1smwhO7g'
-sb= 'value': 'UeWhaDoLoyGTVClbad8Udd_j'
-xs= 'value': '15%3ALhx23MrGqEeS6A%3A2%3A1755440689%3A-1%3A-1'
-
-datr=MPuhaDhKwXKUJA8oUX3e1pJk; sb=MPuhaMtmPpiCiNUnK2uR3rUe; dpr=1.25; locale=zh_TW; ps_l=1; ps_n=1; c_user=100001035739008; xs=14%3AATv4StKkQMqeGg%3A2%3A1755446280%3A-1%3A-1; fr=0avWJNzkh86n5ktow.AWfZmZlOT-wShHnZWIYd2-ZKM_Jy1deYwe9LWpaNkxYqNSxaI9Y.Boofsw..AAA.0.0.BoofwL.AWdSggjE7YlhoXXd7qdyNfyzl2E; presence=C%7B%22t3%22%3A%5B%5D%2C%22utc3%22%3A1755446286366%2C%22v%22%3A1%7D; wd=482x651
-'''
-
-input("請按Enter鍵繼續...") # 等待使用者按Enter鍵
+    
 
 
